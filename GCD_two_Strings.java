453631c3c9ff4fc427bf4d2c6a8a07365facef7d
Sai Ganesh Darni <saiganeshdharni48@gmail.com>
2023-12-22 16:03:54 +0530

    Added Comments to Greatest Common Divisor of Two Strings Solution

--- GCD_two_Strings.java
+++ GCD_two_Strings.java
@@ -1,10 +1,11 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
         if(!(str1+str2).equals(str2+str1)){
+            //checks whether two strings form common divisor or not
             return new String();
         }
         else{
-            int gcd = gcd(str1.length(),str2.length());
+            int gcd = gcd(str1.length(),str2.length()); //returns the gcd of two strings which will be the length of the common divisor of two strings
             return str1.substring(0,gcd);
         }
     }
